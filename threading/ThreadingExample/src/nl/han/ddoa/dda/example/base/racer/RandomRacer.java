f7519a604b1770f008b33bdaf1a972d1958ad90d
Rody Middelkoop <r.r.middelkoop@avisi.nl>
2012-09-02 20:07:09 +0200

    Back to the clean files, without the implementation. Javadoc show where the code is heading.

--- threading/ThreadingExample/src/nl/han/ddoa/dda/example/base/racer/RandomRacer.java
+++ threading/ThreadingExample/src/nl/han/ddoa/dda/example/base/racer/RandomRacer.java
@@ -1,18 +1,14 @@
 package nl.han.ddoa.dda.example.base.racer;
 
+/**
+ * Show the differences between start and run.
+ */
 public class RandomRacer
 {
-    /**
-     * @param args
-     */
     public static void main(String[] args)
     {
         Racer rr1 = new Racer();
         Racer rr2 = new Racer();
-        Thread t1 = new Thread(rr1);
-        Thread t2 = new Thread(rr2);
-        t1.start();
-        t2.start();
     }
 
 }
