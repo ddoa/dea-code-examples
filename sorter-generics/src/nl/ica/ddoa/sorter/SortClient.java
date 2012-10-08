72a7e635cbfad06bfa290eb76242d3d5e4df87e5
Rody Middelkoop <rody.middelkoop@han.nl>
2012-10-09 00:29:38 +0200

    Improved instruction about source folders

---
+++ sorter-generics/src/nl/ica/ddoa/sorter/SortClient.java
@@ -0,0 +1,16 @@
+package nl.ica.ddoa.sorter;
+
+/**
+ * Description for the class SortClient:
+ * <p/>
+ * Example usage:
+ * <p/>
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author mdkr
+ * @version Copyright (c) 2012 HAN University, All rights reserved.
+ */
+public class SortClient {
+}
