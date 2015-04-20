de0215ae874c8dba5afda60d6a1db950cd675555
Rody Middelkoop <rody.middelkoop@gmail.com>
2015-04-21 00:09:04 +0200

    Moved files to separate directory

---
+++ android-deckard-unittest/maven/src/main/java/com/example/activity/IDataController.java
@@ -0,0 +1,17 @@
+package com.example.activity;
+
+/**
+ * Description for the class IDataController:
+ * <p/>
+ * Example usage:
+ * <p/>
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author mdkr
+ * @version Copyright (c) 2012 HAN University, All rights reserved.
+ */
+public interface IDataController {
+    String getData();
+}
