70d799c9565d15cb7afd3de41bfac6fa0436b3b3
Rody Middelkoop <rody.middelkoop@gmail.com>
2015-06-17 13:02:33 +0200

    Example results of exercise using JAX RS, Jersey and Jackson

--- jersey-rest/src/main/java/oose/dea/services/rest/ItemRestService.java
+++ jersey-rest/src/main/java/oose/dea/services/rest/ItemRestService.java
@@ -1,22 +1,53 @@
-
 package oose.dea.services.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.inject.Singleton;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/items")
-public class ItemRestService  {
+@Singleton
+public class ItemRestService {
+    private List<Item> items = new ArrayList<Item>();
+
+    public ItemRestService()
+    {
+        items.add(new Item("Bread", "Breakfast", "Delicious!"));
+        items.add(new Item("Butter", "Breakfast", "Use it with bread"));
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTextItems()
+    {
+        return "bread, butter";
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Item> findAll() {
-        List<Item> items = new ArrayList<Item>();
-        items.add(new Item("Frikandel", "Snacks", "Restafval, maar wel lekker"));
-        items.add(new Item("Bradwurst", "Snacks", "Restafval, maar dan op zijn Duits"));
+    public List<Item> getJsonItems()  {
         return items;
+    }
+
+    @GET
+    @Path("/{sku}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Item getJsonItem(@PathParam("sku") final String sku) {
+        return items.stream().filter((item) -> item.getSku().equals(sku)).findAny().get();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addItem(Item item)
+    {
+        items.add(item);
+    }
+
+    @DELETE
+    @Path("/{sku}")
+    public void deleteItem(@PathParam("sku") final String sku)
+    {
+        items.removeIf(item -> item.getSku().equals(sku));
     }
 }
