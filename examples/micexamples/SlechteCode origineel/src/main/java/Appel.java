b5b311b9499658a32e6e5f6518a63c5b717bf7ae
MichelKoolwaaij <michel.koolwaaij@han.nl>
2016-09-30 15:05:44 +0200

    Slechte code voorbeeld
    Dit is een voorbeeld met code die niet voldoet aan SOLID. De opdracht
    is om deze te refactoren zodat deze wel voldoet.

---
+++ examples/micexamples/SlechteCode origineel/src/main/java/Appel.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Michel Koolwaaij on 29-09-16.
+ */
+public class Appel {
+    private double prijs=2;
+
+    public double getPrijs() {
+        return prijs;
+    }
+}
---
+++ examples/micexamples/SlechteCode origineel/src/main/java/Tomaat.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Michel Koolwaaij on 30-09-16.
+ */
+public class Tomaat  {
+    private double prijs;
+    public Tomaat() {
+        prijs = 6;
+    }
+
+    public double getPrijs() {
+        return prijs;
+    }
+}
